3253adac6c0cd1921b15b09a0d5e89512cb8d699
Naveen <naveenramlu28@gmail.com>
2025-08-08 01:55:44 +0530

    #87 Update Jenkinsfile

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -12,8 +12,10 @@ def call(String stageName, String imageTag, String ecrRepoName, String awsAccoun
             sh """
                 aws configure set aws_access_key_id \$AWS_ACCESS_KEY_ID
                 aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY
+                
                 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${awsAccountId}.dkr.ecr.${region}.amazonaws.com
-                docker tag ${imageTag} ${fullTag}
+                
+                docker tag ${ecrRepoName}:${imageTag} ${fullTag}
                 docker push ${fullTag}
             """
         }
