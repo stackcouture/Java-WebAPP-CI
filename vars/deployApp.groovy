7ac2be2e80a3d9f53f00cacd79e3242a77f5b4eb
Naveen <naveenramlu28@gmail.com>
2025-08-09 14:46:00 +0530

    #130 Update Jenkinsfile

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,6 +1,19 @@
 def call() {
-    stage('Deploy') {
-        withMaven(globalMavenSettingsConfig: 'maven-setting-javaapp', jdk: 'Jdk17', maven: 'Maven3', mavenSettingsConfig: '', traceability: true) {
+    script {
+        def gitTag = sh(script: 'git describe --tags --abbrev=0', returnStdout: true).trim()
+        def gitSha = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+        def snapshotVersion = "${gitTag.replaceAll('^v', '')}-${gitSha}-SNAPSHOT"
+
+        sh "mvn versions:set -DnewVersion=${snapshotVersion}"
+        sh "mvn versions:commit"
+
+        withMaven(
+            globalMavenSettingsConfig: 'maven-setting-javaapp',
+            jdk: 'Jdk17',
+            maven: 'Maven3',
+            mavenSettingsConfig: '',
+            traceability: true
+        ) {
             sh 'mvn deploy -DskipTests=true'
         }
     }
