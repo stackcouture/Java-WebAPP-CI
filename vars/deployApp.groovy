4620b2507c59a0b55694fd6b9fd9d5bc9eca59a9
Naveen <naveenramlu28@gmail.com>
2025-08-09 14:50:10 +0530

    #131 Update Deploy groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,20 +1,11 @@
 def call() {
-    script {
-        def gitTag = sh(script: 'git describe --tags --abbrev=0', returnStdout: true).trim()
-        def gitSha = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-        def snapshotVersion = "${gitTag.replaceAll('^v', '')}-${gitSha}-SNAPSHOT"
-
-        sh "mvn versions:set -DnewVersion=${snapshotVersion}"
-        sh "mvn versions:commit"
-
-        withMaven(
-            globalMavenSettingsConfig: 'maven-setting-javaapp',
-            jdk: 'Jdk17',
-            maven: 'Maven3',
-            mavenSettingsConfig: '',
-            traceability: true
-        ) {
-            sh 'mvn deploy -DskipTests=true'
-        }
+    withMaven(
+        globalMavenSettingsConfig: 'maven-setting-javaapp',
+        jdk: 'Jdk17',
+        maven: 'Maven3',
+        mavenSettingsConfig: '',
+        traceability: true
+    ) {
+        sh 'mvn deploy -DskipTests=true'
     }
 }
