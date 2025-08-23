e137654a6a71475d1b9effb42b17bd3043d45036
Naveen <naveenramlu28@gmail.com>
2025-08-23 22:36:03 +0530

    #modified with pom.xml

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase; 
 
 @SpringBootTest
 @ActiveProfiles("test")
