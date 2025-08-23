a982d2f76502ae695afb114f0e51547710330362
Naveen <naveenramlu28@gmail.com>
2025-08-23 22:15:01 +0530

    #201 Update the properties in test and pom.xml

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DemoApplicationTests {
 
     @Test
